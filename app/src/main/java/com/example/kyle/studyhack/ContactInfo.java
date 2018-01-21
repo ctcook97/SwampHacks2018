a67546721f052a42afeb67ee4f8f55f6131c45de
Unknown <ctcook97@gmail.com>
2018-01-21 04:22:59 -0500

    Updated contact info. If we run out of time hard code some text in

--- app/src/main/java/com/example/kyle/studyhack/ContactInfo.java
+++ app/src/main/java/com/example/kyle/studyhack/ContactInfo.java
@@ -3,6 +3,9 @@ package com.example.kyle.studyhack;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ContactInfo extends AppCompatActivity {
 
@@ -10,6 +13,16 @@ public class ContactInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_info);
+        TextView Name = (TextView) findViewById(R.id.Name);
+        Name.setText("Cameron Cook");
+        TextView Number = (TextView) findViewById(R.id.Number);
+        Number.setText("352-226-6073");
+        Button Leave = (Button) findViewById(R.id.Leave);
+        Leave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), classes.class));
+            }
+        });
     }
-
 }
