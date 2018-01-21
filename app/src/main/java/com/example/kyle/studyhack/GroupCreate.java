7defb8c14bb753288e5a04017af1f9b827f194e3
Matt Hannan <matthewh2@ufl.edu>
2018-01-21 02:11:30 -0500

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/com/example/kyle/studyhack/GroupCreate.java
+++ app/src/main/java/com/example/kyle/studyhack/GroupCreate.java
@@ -1,13 +1,27 @@
 package com.example.kyle.studyhack;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GroupCreate extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_group_create);
-    }
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            setContentView(R.layout.activity_group_create);
+
+            final Button button = findViewById(R.id.createGroupButton);
+            button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Intent myIntent = new Intent(GroupCreate.this, ContactInfo.class);
+                    startActivity(myIntent);
+                }
+            });
+        }
 }
+
